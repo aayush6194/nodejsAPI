d811b8a7250d72b9df13301d2ab69c66ddeab6e2
aayush6194 <aayush6194@gmail.com>
2019-03-18 22:37:55 -0500

    done

--- index.js
+++ index.js
@@ -7,41 +7,6 @@ var port = process.env.PORT || 3000;
 const multer = require('multer');
 
 const app = express();
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
-//const db = require("./db");
-
-// db.connect("", {name: String, email: String,message: String
-//               //,  img: { data: Buffer, contentType: String }
-//             });
-
-const storage = new GridFsStorage({
- url: "mongodb://aayush6194:poop12@ds041571.mlab.com:41571/portfolio",
- file: (req, file) => {
-   return new Promise((resolve, reject) => {
-       const filename = file.originalname;
-       const fileInfo = {
-         filename: filename,
-         bucketName: 'uploads'
-       };
-       resolve(fileInfo);
-   });
- }
-});
-
-const upload = multer({ storage });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
-
-app.post('/upload', upload.single('file'),  (req, res) => {
- res.redirect('https://www.aayushh.com/Message/');
-});
 
 app.get('/',  (req, res) => {
  res.send({active: true});
