b55431248b0dee789a76f326eec196e70a3751c9
aayush6194 <aayush6194@gmail.com>
2019-03-18 22:41:38 -0500

    done

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 var port = process.env.PORT || 3000;
 
 
+const multer = require('multer');
+
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+
+const storage = new GridFsStorage({
+  url: "mongodb://aayush6194:poop12@ds041571.mlab.com:41571/portfolio",
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+        const filename = file.originalname;
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+    });
+  }
+});
+
 app.get('/',  (req, res) => {
  res.send({active: true});
 });
