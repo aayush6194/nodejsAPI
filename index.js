05f90a6f95adcb5ea03534ce49b54ae8e52502dc
aayush6194 <aayush6194@gmail.com>
2019-03-18 22:39:14 -0500

    done

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser"); //post request
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 var port = process.env.PORT || 3000;
-const multer = require('multer');
 
-const app = express();
 
 app.get('/',  (req, res) => {
  res.send({active: true});
