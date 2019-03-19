52af7964dfd13a6fc5aa95ebbfd952af824dc9d8
aayush6194 <aayush6194@gmail.com>
2019-03-19 00:25:40 -0500

    file request added

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ var app = express();
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser"); //post request
 var urlencodedParser = bodyParser.urlencoded({extended: false});
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3002;
 
 
 const multer = require('multer');
 
 const GridFsStorage = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
+
+const conn = mongoose.createConnection("mongodb://aayush6194:poop12@ds041571.mlab.com:41571/portfolio");
+let gfs;
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
 
 const storage = new GridFsStorage({
   url: "mongodb://aayush6194:poop12@ds041571.mlab.com:41571/portfolio",
@@ -27,6 +34,12 @@ const storage = new GridFsStorage({
 
 const upload = multer({ storage });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/',  (req, res) => {
  res.send({active: true});
 });
@@ -35,5 +48,18 @@ app.post('/upload', upload.single('file'),  (req, res) => {
   res.redirect('https://www.aayushh.com/Message/');
 });
 
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: 'No files exist'
+      });
+    }
+
+    // Files exist
+    return res.send({num: files.length});
+  });
+});
 
 app.listen(port);
